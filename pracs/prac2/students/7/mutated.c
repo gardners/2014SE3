6b97c3428f45ceba95ff363468e3563e6ddcd54d
gardners <paul@servalproject.org>
2014-03-20 13:46:48 +1030

    Uninformative commit message

--- pracs/prac2/students/7/mutated.c
+++ pracs/prac2/students/7/mutated.c
@@ -53,8 +53,8 @@ int main(int argc, char *argv[])
 	socklen_t sin_len = sizeof(cli_addr);
  
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0){
  		return -1;}
+	if (sock < 0){
 
 	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
 	
