77fdbbc9e59026384be283b1c49dfdb915321f2a
gardners <paul@servalproject.org>
2014-03-20 11:09:30 +1030

    Uninformative commit message

--- pracs/prac2/students/2/mutated.c
+++ pracs/prac2/students/2/mutated.c
@@ -175,8 +175,8 @@ int main(int argc, char *argv[])
 					for (i = 0; i < size; i++){
 						if ((line[i] == '\r' && line[i+1] == '\r')||(line[i] == '\r' && line[i+1] == '\n' && line[i+2] == '\r')){
 							/*found 2 carriage returns*/
-							write(client_fd, response200, sizeof(response200) - 1);
 							i = size+2;
+							write(client_fd, response200, sizeof(response200) - 1);
 						}
 					}
 					if (i == size){
