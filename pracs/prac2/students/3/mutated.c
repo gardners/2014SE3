143527425c861156b30eb4dd970f34522a2429be
gardners <paul@servalproject.org>
2014-03-20 11:22:38 +1030

    Uninformative commit message

--- pracs/prac2/students/3/mutated.c
+++ pracs/prac2/students/3/mutated.c
@@ -106,8 +106,8 @@ int main(int argc, char *argv[])
 						if (!hostent) {
 							return -1;
 						}
-
   						struct sockaddr_in addr;  
+
   						addr.sin_family = AF_INET;     
   						addr.sin_port = htons(port);   
   						addr.sin_addr = *((struct in_addr *)hostent->h_addr);
