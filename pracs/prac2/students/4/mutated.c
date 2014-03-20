e78749e859f52da1aac534a7af63135f6f6c3b32
gardners <paul@servalproject.org>
2014-03-20 11:58:51 +1030

    Uninformative commit message

--- pracs/prac2/students/4/mutated.c
+++ pracs/prac2/students/4/mutated.c
@@ -61,8 +61,8 @@ int main(int argc, char *argv[])
 	svr_addr.sin_family = AF_INET;
 	svr_addr.sin_addr.s_addr = INADDR_ANY;
 	svr_addr.sin_port = htons(port);
- 
 	if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+ 
 		close(sock);
 		return -1;
 	}
