1d45f0ddff7582ea241b2c271dd8456631217e5c
gardners <paul@servalproject.org>
2014-03-20 11:21:54 +1030

    Uninformative commit message

--- pracs/prac2/students/3/mutated.c
+++ pracs/prac2/students/3/mutated.c
@@ -161,8 +161,8 @@ int main(int argc, char *argv[])
 							/* this server only handles HTTP 200 responses, so if we're sent anything different, we hand down a 500 error to the client.*/
 							else {
 								write(client_fd,response500,strlen(response500));
-							}							
 						}
+							}							
 						else{
 							printf("\ntcp port did not respond\n");
 						}
