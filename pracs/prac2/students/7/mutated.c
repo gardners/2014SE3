2ad018a119c7fffa511721c272da6fb0e8071dd5
gardners <paul@servalproject.org>
2014-03-20 13:46:27 +1030

    Uninformative commit message

--- pracs/prac2/students/7/mutated.c
+++ pracs/prac2/students/7/mutated.c
@@ -208,8 +208,8 @@ int main(int argc, char *argv[])
 						}
 					}
 				else if (line[5] == ' ' && line[6] == 'H' && line[7] == 'T' && line[8] == 'T' &&  line[9] == 'P' && line[10] == '/' && line[11] == '1' && line[12] == '.' && line[13] == '1'){
-				}
 					printf("\nThe client has requested the wrong version of HTTP. Sending error.\n");
+				}
 					write(client_fd, response400, sizeof(response400) - 1);
 				}
 				else if (line[5] == ' ' && line[6] == 'F' && line[7] == 'T' && line[8] == 'P' && line[9] == '/'){
