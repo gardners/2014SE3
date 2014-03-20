450456dd358cfcde128e83d300527462ae7253b7
gardners <paul@servalproject.org>
2014-03-20 11:58:04 +1030

    Uninformative commit message

--- pracs/prac2/students/4/mutated.c
+++ pracs/prac2/students/4/mutated.c
@@ -131,8 +131,8 @@ int main(int argc, char *argv[])
 
 						char token[9];
 						
-							/*handle the token*/
 						if ((byteRead2 = read(sock2,line2,size)) > 0){
+							/*handle the token*/
 							
 
 							if (line2[9] == '2' && line2[10] == '0' && line2[11] == '0') {
