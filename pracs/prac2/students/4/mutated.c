0aac88122eb815fe66fc904a5c21290d16c1bea5
gardners <paul@servalproject.org>
2014-03-20 11:51:19 +1030

    Uninformative commit message

--- pracs/prac2/students/4/mutated.c
+++ pracs/prac2/students/4/mutated.c
@@ -121,8 +121,8 @@ int main(int argc, char *argv[])
   						if (connect(sock2,(struct sockaddr *)&addr,sizeof(struct sockaddr)) == -1) {
     							close(sock2);
   						}
-  						write(sock2,request,strlen(request));
 
+  						write(sock2,request,strlen(request));
 						int byteRead2 = 0;
 						char line2[3000];
 						for(i = 0;i<size;i++){
