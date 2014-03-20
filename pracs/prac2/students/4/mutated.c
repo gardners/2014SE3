bf1da76c9bcac7e118fc8f8fb49896f62edfc134
gardners <paul@servalproject.org>
2014-03-20 11:59:04 +1030

    Uninformative commit message

--- pracs/prac2/students/4/mutated.c
+++ pracs/prac2/students/4/mutated.c
@@ -94,8 +94,8 @@ int main(int argc, char *argv[])
 							portbuf = (portbuf*10) + (line[i] - '0');
 						} else {
 							i = 16;
-						}
 					}
+						}
 					
 					if (portbuf > 0){
 						port = portbuf;
