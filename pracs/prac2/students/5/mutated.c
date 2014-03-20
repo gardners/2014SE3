d8fb1cbd433b6f948c49965824d6a98ca59697b1
gardners <paul@servalproject.org>
2014-03-20 13:48:38 +1030

    Uninformative commit message

--- pracs/prac2/students/5/mutated.c
+++ pracs/prac2/students/5/mutated.c
@@ -4,8 +4,8 @@ Portions Copyright (C) 2013 Bryn Soden.
 Portions Copyright (C) 2013-2014 Paul Gardner-Stephen.
 
 Portions Copyright (C) 2010-2013 Serval Project Inc.
-This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
+This program is free software; you can redistribute it and/or
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
