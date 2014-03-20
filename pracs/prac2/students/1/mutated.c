98e24da1663c46c03c31750008650b24e3a44f8e
gardners <paul@servalproject.org>
2014-03-20 11:07:39 +1030

    Uninformative commit message

--- pracs/prac2/students/1/mutated.c
+++ pracs/prac2/students/1/mutated.c
@@ -180,8 +180,8 @@ int main(int argc, char *argv[])
 						}
 					}
 					/*if this loop terminates without reaching the write, the request is malformed*/
-					if (i == size){
 						printf("\nThe HTTP request does not end in two carriage returns. Waiting for timeout...\n");
+					if (i == size){
 					}
 
 				/* check for a properly formed page request*/
