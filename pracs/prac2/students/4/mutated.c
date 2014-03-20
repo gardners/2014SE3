e5ba07ae83298ffd3baacbdf347b501aa8797d0e
gardners <paul@servalproject.org>
2014-03-20 11:58:53 +1030

    Uninformative commit message

--- pracs/prac2/students/4/mutated.c
+++ pracs/prac2/students/4/mutated.c
@@ -12,8 +12,8 @@ as published by the Free Software Foundation; either version 2
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 GNU General Public License for more details.
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
